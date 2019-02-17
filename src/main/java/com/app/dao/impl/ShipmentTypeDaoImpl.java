c44db75ce82d0ec083fb28341e377b8cf6b7f9ff
javabyraghu <javabyraghu@gmail.com>
2019-02-17 13:25:07 +0530

    ShpmentTypeDaoImpl Detached Criteria Update

--- src/main/java/com/app/dao/impl/ShipmentTypeDaoImpl.java
+++ src/main/java/com/app/dao/impl/ShipmentTypeDaoImpl.java
@@ -3,6 +3,9 @@ package com.app.dao.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
@@ -18,21 +21,15 @@ public class ShipmentTypeDaoImpl implements IShipmentTypeDao {
 	private HibernateTemplate ht;
 
 	public Integer saveShipmentType(ShipmentType shipmentType) {
-		
 		return (Integer) ht.save(shipmentType);
-		 
 	}
 
 	public void updateShipmentType(ShipmentType shipmentType) {
-		
 		ht.update(shipmentType);
-
 	}
 
 	public void deleteShipmentType(Integer shipmentId) {
-		
 		ht.delete(new ShipmentType(shipmentId));
-
 	}
 
 	public ShipmentType getShipmentTypeById(Integer shipmentId) {
@@ -43,34 +40,57 @@ public class ShipmentTypeDaoImpl implements IShipmentTypeDao {
 		return ht.loadAll(ShipmentType.class);
 	}
 
-	@SuppressWarnings({ "deprecation", "unchecked" })
+	@SuppressWarnings("unchecked")
 	public List<Object[]> getShipmentTypeCount() {
 
-		String hql =   " select shipmentMode,count(shipmentMode) from "
+		/*String hql =   " select shipmentMode,count(shipmentMode) from "
 				     + ShipmentType.class.getName()
-				     + " group by shipmentMode";
-		List<Object[]> shipmentTypes=(List<Object[]>) ht.find(hql);
+				     + " group by shipmentMode";*/
+		DetachedCriteria hql=
+				DetachedCriteria.forClass(ShipmentType.class)
+				.setProjection(
+						Projections.projectionList()
+						.add(Projections.groupProperty("shipmentMode"))
+						.add(Projections.count("shipmentMode"))
+						);
+		List<Object[]> shipmentTypes=(List<Object[]>) ht.findByCriteria(hql);
 		
 		return shipmentTypes;
 	}
 
-	@SuppressWarnings({ "unchecked", "deprecation" })
+	@SuppressWarnings("unchecked")
 	public boolean isShipmentCodeExist(String shipmentCode) {
 		
 		long count=0;
-		String hql="select count(shipmentCode) from "+ShipmentType.class.getName()+" where shipmentCode=?";
-		List<Long> shipmentTypes=(List<Long>) ht.find(hql, shipmentCode);
+		//String hql="select count(shipmentCode) from "+ShipmentType.class.getName()+" where shipmentCode=?";
+		DetachedCriteria hql=
+				DetachedCriteria.forClass(ShipmentType.class)
+				.setProjection(Projections.count("shipmentCode"))
+				.add(Restrictions.eq("shipmentCode", shipmentCode))
+				;
+		List<Long> shipmentTypes=(List<Long>) ht.findByCriteria(hql);
 		if (shipmentTypes!=null && !shipmentTypes.isEmpty()) {
 			count=shipmentTypes.get(0);
 		}
 		return count>0?true:false;
 	}
 
-	@SuppressWarnings({ "deprecation", "unchecked" })
+	@SuppressWarnings("unchecked")
 	public Map<Integer, String> getEnableShipmentIdsAndCodes() {
 		
-		String hql = "select shipmentid,shipmentCode from "+ShipmentType.class.getName()+" where enableShipment=?";
-		return AppCollectionUtil.toMap((List<Object[]>) ht.find(hql, "YES"));
+		//String hql = "select shipmentid,shipmentCode from "+ShipmentType.class.getName()+" where enableShipment=?";
+		
+		DetachedCriteria hql=
+				DetachedCriteria.forClass(ShipmentType.class)
+				.setProjection(
+						Projections.projectionList()
+						.add(Projections.property("shipmentid"))
+						.add(Projections.property("shipmentCode"))
+						)
+				.add(Restrictions.eq("enableShipment", "YES"));
+
+		List<Object[]> list=(List<Object[]>)ht.findByCriteria(hql);
+		return AppCollectionUtil.toMap(list);
 	}
 
 }
